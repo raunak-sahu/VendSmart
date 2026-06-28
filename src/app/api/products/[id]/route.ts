847e439af687775c9c76173d7bcbf50326d10028
Raunak Sahu <raunaksahu765@gmail.com>
2026-06-28 17:36:34 +0530

    Added product update API

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -17,16 +17,66 @@ export async function GET(
   }
 ) {
   const product =
-    await prisma.product.findUnique(
-      {
-        where: {
-          id: params.id,
-        },
-      }
-    );
+    await prisma.product.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
 
   return NextResponse.json(
     product
+  );
+}
+
+export async function PUT(
+  req: Request,
+  {
+    params,
+  }: {
+    params: {
+      id: string;
+    };
+  }
+) {
+  const body =
+    await req.json();
+
+  const updatedProduct =
+    await prisma.product.update({
+      where: {
+        id: params.id,
+      },
+      data: {
+        productName:
+          body.productName,
+
+        category:
+          body.category,
+
+        costPrice:
+          Number(
+            body.costPrice
+          ),
+
+        sellingPrice:
+          Number(
+            body.sellingPrice
+          ),
+
+        currentStock:
+          Number(
+            body.currentStock
+          ),
+
+        minimumStockThreshold:
+          Number(
+            body.minimumStockThreshold
+          ),
+      },
+    });
+
+  return NextResponse.json(
+    updatedProduct
   );
 }
 
@@ -40,13 +90,11 @@ export async function DELETE(
     };
   }
 ) {
-  await prisma.product.delete(
-    {
-      where: {
-        id: params.id,
-      },
-    }
-  );
+  await prisma.product.delete({
+    where: {
+      id: params.id,
+    },
+  });
 
   return NextResponse.json({
     success: true,
